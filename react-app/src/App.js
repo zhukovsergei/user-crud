d30284dd584382e35ef9e70c89f585cc36dd274a
Zhukov Sergei <alivesystemsonly@gmail.com>
2024-05-19 08:25:55 +0200

    react app

---
+++ react-app/src/App.js
@@ -0,0 +1,25 @@
+import './App.css';
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import Navbar from "./layout/Navbar";
+import Home from "./pages/Home";
+import AddUser from "./users/AddUser";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+function App() {
+  return (
+    <div className="App">
+        <Router>
+            <Navbar />
+
+            <Routes>
+                <Route exact path="/" element={<Home />} />
+                <Route exact path="/adduser" element={<AddUser />} />
+                {/*<Route exact path="/edituser/:id" element={<EditUser />} />*/}
+                {/*<Route exact path="/viewuser/:id" element={<ViewUser />} />*/}
+            </Routes>
+        </Router>
+    </div>
+  );
+}
+
+export default App;
